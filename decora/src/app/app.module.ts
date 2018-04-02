34cbc6bb26669ad8ced5fbe1fb6574432f7fb43d
Alexandre Nunes <alexandregarcezn@gmail.com>
2018-04-02 17:33:21 -0300

    Implementacao das telas e esquema de rotas

--- decora/src/app/app.module.ts
+++ decora/src/app/app.module.ts
@@ -2,33 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
  
-import {MatButtonModule, MatGridListModule, MatInputModule, MatIconModule} from '@angular/material';
+import {MatButtonModule, MatGridListModule, MatInputModule, MatIconModule, MatCardModule} from '@angular/material';
  
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
-import { AuthComponent } from './auth/auth.component';
-import { UsersComponent } from './users/users.component';
-import { UsersRegisterComponent } from './users/users-register/users-register.component';
-import { UsersViewComponent } from './users/users-view/users-view.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
  
 @NgModule({
   declarations: [
       AppComponent,
       HeaderComponent,
       LoginComponent,
-      AuthComponent,
-      UsersComponent,
-      UsersRegisterComponent,
-      UsersViewComponent
+      HomeComponent,
+      RegisterComponent
     ],
     imports: [
       BrowserModule,
       BrowserAnimationsModule,
+      routing,
       MatButtonModule,
       MatGridListModule,
       MatInputModule,
-      MatIconModule
+      MatIconModule,
+      MatCardModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- decora/src/app/app.routing.ts
+++ decora/src/app/app.routing.ts
@@ -1,16 +1,22 @@
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
-import { LoginComponent } from "app/login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 const APP_ROUTES: Routes = [
     {     
         path: '', 
-        component: LoginComponent,
+        component: HomeComponent
     },
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     }
 ];
 
